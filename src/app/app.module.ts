4a7de5490f274517793f7855fc321e11579d9c03
tay cavett <taycavett98@gmail.com>
2022-12-12 10:08:42 -0600

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes-list/recipes-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShopppingEditComponent } from './shopping-list/shoppping-edit/shoppping-edit.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipesListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShopppingEditComponent
+    RecipesListComponent
   ],
   imports: [
     BrowserModule
